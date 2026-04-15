56f6c6a19436b9e4aa1f75084b0934d0e6f251a2
Jwilfeard <Jwilfeard2@gmail.com>
2026-04-14 20:47:16 -0400

    Create EventDetails component for the Over-Under-December-Doubles tournament

---
+++ src/components/EventDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const EventDetails = () => {
+    return (
+        <div>
+            <h1>Over/Under 50 Doubles Scratch Tournament</h1>
+            <p><strong>Date:</strong> December 20, 2026</p>
+            <p><strong>Location:</strong> ABC Gates Bowl, 645 Spencerport Rd, Rochester, NY 14606</p>
+            <p><strong>Entry Fee:</strong> $200</p>
+            <p><strong>Maximum Teams:</strong> 72</p>
+            <p><strong>Registration Deadline:</strong> December 7, 2026</p>
+            <p><strong>Complete Rules:</strong> <a href="https://github.com/Jwilfeard/Over-Under-December-Doubles">Click Here</a></p>
+        </div>
+    );
+};
+
+export default EventDetails;
