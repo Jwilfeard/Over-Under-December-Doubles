70a3f1b3a015c4875dcde3b265a6c68e19dfd4ea
Jwilfeard <Jwilfeard2@gmail.com>
2026-04-14 20:39:43 -0400

    Create App.js for tournament sign-up website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Over/Under 50 Doubles Scratch Tournament</h1>
+      {/* Add components here */}
+    </div>
+  );
+}
+
+export default App;
